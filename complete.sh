9bad10c4137eb483020866184b9bd7e14d81a5f7
taishi <791974929@qq.com>
2017-05-11 22:52:04 +0800

    2017.05.11

--- complete.sh
+++ complete.sh
@@ -1,165 +1,5 @@
 #!/bin/bash
 #模式一：分区并WDCP数据迁移
-# ↓ 磁盘挂载on/off
-mount_state(){
-	start(){
-		# ↓ 读取磁盘挂载信息
-		disk_sda_dfh=`df -h | grep $1 | awk '{print $1}'`
-		# ↓ 判断磁盘是否挂载，若为空就执行挂载，反则不执行
-		if [ "$disk_sda_dfh" = "" ]; then
-			# ↓ $2代表WDCP的目录，默认/www
-			`mount $1"1" $2`
-			# ↓ 读取磁盘挂载信息
-			disk_sdb_dfh=`df -h | grep $1 | awk '{print $1}'`
-			# ↓ 判断磁盘是否挂载，若为空则挂载失败，反则成功
-			if [ "disk_sdb_dfh" != "" ]; then
-				# ↓ 成功
-				echo "磁盘挂载成功!"
-				pid=1
-			else 
-				# ↓ 失败
-				echo "磁盘挂载失败!"
-				pid=0
-			fi 
-		else 
-			# ↓ 磁盘已经挂载
-			echo "磁盘已经挂载!"
-			pid=0
-		fi 
-		return $pid
-	}
-	stop(){
-		# ↓ 读取磁盘挂载信息
-		disk_sda_dfh=`df -h | grep $1 | awk '{print $1}'`
-		# ↓ 判断磁盘是否挂载，不为空就执行解除挂载，反则不执行
-		if [ "$disk_sda_dfh" != "" ];then 
-			# ↓ 获取磁盘所挂载的目录
-			disk_sde_dfh=`df -h | grep $1 | awk '{print $6}'`
-			# ↓ 判断磁盘挂载的目录是否有效
-			if [ "$disk_sde_dfh" != "" ]; then
-				# ↓ 取消该磁盘的挂载
-				`umount $disk_sde_dfh`
-				disk_sda_dfh=`df -h | grep $1 | awk '{print $1}'`
-				if [ "$disk_sda_dfh" = "" ]; then 
-					# ↓ 打印成功的消息
-					echo "取消挂载成功!"
-					pid=1
-				else
-					# ↓ 打印失败的消息
-					echo "取消挂载失败!"
-					echo "请结束$disk_sde_dfh目录下正在运行的程序"
-					pid=0
-				fi 
-			else
-				echo "没有找到磁盘挂载的目录，难道这是一个错误？"
-				pid=0
-			fi 
-		else
-			# ↓ 打印磁盘没有挂载的消息 
-			echo "磁盘没有挂载"
-			pid=1
-		fi 
-		return $pid
-	}
-	status(){
-		disk_sda_dfh=`mount | grep $1`
-		if [ "$disk_sda_dfh" = "" ]; then
-			echo "磁盘没有挂载！"
-			pid=0
-		else
-			echo "$disk_sda_dfh"
-			pid=1
-		fi 
-		return $pid
-	}
-	case "$1" in
-		start)
-			start $2
-			;;
-		stop)
-			stop $2
-			;;
-		status)
-			status $2
-			;;
-		*)
-			echo $"Usage: $prog {start|stop|status}"
-			RETVAL=1
-	esac
-}
-# ↓ 扩展磁盘
-parted_state(){
-# ↓ 判断系统是否安装parted函数
-status_parted(){
-	yum_install_parted=`yum list installed | grep "parted"`
-	if [ "$yum_install_parted" = "" ]; then 
-		`yum -y install parted`
-		yum_install_parted=`yum list installed | grep "parted"`
-		if [ "$yum_install_parted" = "" ]; then 
-			echo "安装失败"
-		else
-			echo "安装成功"
-			# disk_yun_parted $1
-		fi
-	else 
-		echo "已经安装，进行下一步!"
-		# disk_yun_parted $1
-	fi
-}
-status (){
-echo "p
-q
-" | parted $1 > xp.txt
-}
-
-start (){
-# ↓ 扩展磁盘的函数
-parted_run(){
-# ↓ 得到磁盘的分区编号 如：/dev/sda1
-disk_sdb_fdisk=`fdisk -l $1 | grep $1[1-9] | awk '{print $1}'`
-# ↓ 获取需要扩展的分区的Start值，没有对齐的原因是加上tab后执行命令会出错，不完美
-echo "p
-q
-" | parted $1 > xp.txt
-disk_sda_parted_two=`cat xp.txt | grep -w ' 1 ' | awk '{print $2}'`
-# ↓ 执行扩展，理由同上 ↑
-echo "rm 1
-mkpart primary $disk_sda_parted_two -1
-q
-" | parted $1
-		e2fsck -f $disk_sdb_fdisk
-		resize2fs $disk_sdb_fdisk
-		#最后删除获取Start值时生成的临时文件
-		rm -rf xp.txt
-	}
-	disk_yun_parted(){
-		# ↓ 获取分区号 如：/dev/sda1
-		disk_sda_dfh=`df -h | grep $1 | awk '{print $1}'`
-		# 判断分区是否挂载，挂载则解除挂载后执行扩展，反则直接执行扩展
-		if [ "$disk_sda_dfh" != "" ]; then 
-			disk_state stop $1
-			parted_run $1
-		else 
-			parted_run $1
-		fi
-	}
-	# disk_yun_parted $1
-}
-	case "$1" in
-		start)
-			start $2
-			;;
-		status)
-			status $2
-			;;
-		status_parted)
-			status_parted
-			;;
-		*)
-			;;
-	esac
-	return $pid
-}
 # ↓ 磁盘分区
 fdisk_state(){
 	# ↓ 检测磁盘是否拥有分区
@@ -217,58 +57,120 @@ fi
 	esac
 return $pid
 }
+# ↓ 针对centos7
+mv_www_centos7(){
+	systemctl stop mysqld
+	systemctl stop httpd 
+	systemctl stop nginxd
+	systemctl stop wdcp
+	systemctl stop wdapache
+	systemctl stop memcached
+	systemctl stop pureftpd
+	if [ ! -d "/xpxp" ]; then
+		mkdir /xpxp
+	fi
+	mount $1"1" /xpxp
+	c=`ps -ef | grep /www`
+	if [ "$c" == "" ];then
+		mv /www/* /xpxp/
+		u=`umount /xpxp`
+		if [ "$u" == "" ];then
+			mount -t ext4 $1"1" /www
+		fi
+	else 
+		echo "未成功运行！"
+	fi
+	# 数据迁移完成,启动服务
+	systemctl start mysqld
+	systemctl start httpd
+	systemctl start nginxd
+	systemctl start wdcp
+	systemctl start wdapache
+	systemctl start memcached
+	systemctl start pureftpd
+	df -h | grep /www
+}
+# ↓ 针对centos6
+mv_www_centos6(){
+	service mysqld stop
+	service httpd stop
+	service nginxd stop
+	service wdcp stop
+	service wdapache stop
+	service memcached stop
+	service pureftpd stop
+	if [ ! -d "/xpxp" ]; then
+		mkdir /xpxp
+	fi
+	mount $1"1" /xpxp
+	c=`ps -ef | grep /www`
+	if [ "$c" == "" ];then
+		mv /www/* /xpxp/
+		u=`umount /xpxp`
+		if [ "$u" == "" ];then
+			mount -t ext4 $1"1" /www
+		fi
+	else 
+		echo "未成功运行！"
+	fi
+	# 数据迁移完成,启动服务
+	service mysqld start
+	service httpd start
+	service nginxd start
+	service wdcp start
+	service wdapache start
+	service memcached start
+	service pureftpd start
+	df -h | grep /www
+}
 if [ "$1" = "" ]; then
 	echo "请带上磁盘名称：如./$0 /dev/sda"
 	exit 0
 fi
+# ↓ 获得系统的名称
+x_release=`cat /etc/redhat-release | awk '{printf $1}'`
+# ↓ 获得系统的版本编号
+x=`cat /etc/redhat-release | awk '{printf $4}' | cut -d "." -f 1`
+if [ "$x_release" != "CentOS" ] || [ "$x" != "6" ] && [ "$x" != "7" ];then 
+	echo "只支持CentOS 6.*/7.*"
+	exit 0
+fi
+
 echo "脚本将自动分区并迁移数据"
-echo "1.磁盘已经分区则不会再分区"
+echo "1.磁盘已经分区则不会执行任何操作"
 echo "2.分区大小一定要比/www目录所占空间大"
 echo "3.请事先备份数据,以免出现错误时数据丢失"
-echo "4.一切的一切与作者无关"
+echo "4.BUG可以来我的留言板留言"
 echo "继续/放弃 (y/n)"
 read hehe
 if [ "$hehe" == "y" ];then 
 	x=`fdisk -l $1 | wc -m`
-	if [ "$x" = "0" ];then
+	if [ "$x" == "0" ];then
 		echo "read error!"
 		exit 0
-	elif [ "$x" = "" ];then
+	elif [ "$x" == "" ];then
 		echo "ERROR:没有这块磁盘，或者输入不正确。例:/dev/sda"
 		exit 0
 	else 
 		echo "好的，脚本将继续运行"
 	fi 
-
-	parted_state status_parted
-
+	# ↓ 交给fdisk_state函数检测磁盘是否有分区,有则返回 1 ,没有则返回 0 
 	fdisk_state status $1
+	# ↓ 接收 fdisk_state 函数的返回值
 	fdisk_pid=$?
-	if [ "$fdisk_pid" = "0" ];then
+	if [ "$fdisk_pid" == "0" ];then
+		# ↓ 由fdisk_state 函数对磁盘进行分区
 		fdisk_state start $1
-	elif [ "$fdisk_pid" = "1" ];then
-		m=`fdisk -l /dev/sdc | grep "/dev/sdc:" | awk '{printf $3 $4}' | cut -f 1 -d ","` 
-		m_one=`fdisk -l /dev/sdc | grep "/dev/sdc:" | awk '{printf $3}' | cut -f 1 -d ","` 
-		x=`du -sh --block-size G /www | awk '{print $1}' | cut -f 1 -d "G"`
-		parted_state status $1
-		disk_sda_parted_two=`cat xp.txt | grep -w ' 1 ' | awk '{print $3}'`
-		rm -rf ./xp.txt
-		if [ "$m" = "$disk_sda_parted_two" ];then
-			if [ "$m_one" > "$x" ];then
-				echo "磁盘分区大小符合要求！"
-			else 
-				echo "Error!"
-			fi
+		
+		if [ "$x" == "7" ];then
+			mv_www_centos7 $1
+		elif [ "$x" == "6" ];then
+			mv_www_centos6 $1
 		else 
-			if [ "$disk_sda_parted_two" > "$x" ];then
-				echo "磁盘分区大小符合要求！"
-			else 
-				echo "Error!"
-			fi
-			# echo "error!"
-			# echo $m
-			# echo $disk_sda_parted_two
+			echo "error!"
 		fi
+	elif [ "$fdisk_pid" == "1" ];then
+		echo "脚本未作任何操作,因为磁盘已经拥有分区"
 	else 
 		echo "error!"
 	fi
